99f95c2b8a1603fee16cda55d1e16d100b87e7a2
yamada99 <r.yamada@unirobot.com>
2022-06-07 15:53:46 +0900

    json受け取り用サーバー

---
+++ jsonServer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#参考：https://qiita.com/tkj/items/1338ad081038fa64cef8
+
+import http.server as s
+import json
+
+class MyHandler(s.BaseHTTPRequestHandler):
+    def do_POST(self):
+
+        # リクエスト取得
+        content_len  = int(self.headers.get("content-length"))
+        body = json.loads(self.rfile.read(content_len).decode('utf-8'))
+
+        # レスポンス処理
+        print(body["test"])
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json;charset=utf-8')
+        self.end_headers()
+        self.wfile.write(body)
+
+# サーバ起動
+host = '0.0.0.0'
+port = 3333
+httpd = s.HTTPServer((host, port), MyHandler)
+httpd.serve_forever()
